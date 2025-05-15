d230511bb3fcc04a2e4c7451b20c1c31c2bd7577
filzafida <filzafida68@gmail.com>
2025-05-15 14:11:57 +0530

    addition

---
+++ add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//this code will be used to add two numbers
+int main(){
+    int num1 = 3, num2 = 6, sum;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number:");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("The sum of %d and %d is %d.\n", num1,num2,sum);
+    return 0;
+}
+//Here we have take the examples of the no.s 3 and 6..
